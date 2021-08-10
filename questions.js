b55d55af58c5da77ab60ec1d6cb4f7d8a22c5afa
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-10 13:44:54 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"...... Laura new to the school? - Yes, she ...... .",
+        options:["Is / is", "Are / are"],
+        answer: 0
+    },
+    {
+        q:"...... Tim in Year 5? - No, he ...... .",
+        options:["Are / aren't", "Is / isn't"],
+        answer: 1
+    },
+    {
+        q:"...... Paul and Steve at secondary school? - Yes, they ...... .",
+        options:["Are / are", "Is / is"],
+        answer: 0
+    },
+    {
+        q:"...... Jane and Claire in Year 7? - No, they ...... . ",
+        options:["Is / isn't", "Are / aren't"],
+        answer: 1
+    },
+    {
+        q:"...... Laura and Jane at primary school? - Yes, they ...... .",
+        options:["Are / are", "Is / is"],
+        answer: 0
+    }
+
+
+
+]
